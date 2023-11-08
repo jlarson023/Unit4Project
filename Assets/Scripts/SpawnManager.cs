10934e393d7d08f9b19ab248362cc17371f7743a
jlarson023 <larsontjonathan@gmail.com>
2023-11-08 10:14:24 -0800

    Day 3
    Finished waves and added that a power up can spawn at different points when a wave is over.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,21 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject powerupPrefab;
     public GameObject enemyPrefab;
     //enemy spawn position variables
-    public float xMin;
-    public float xMax;
-    public float yPos;
+    private float xMin = -9.5f;
+    private float xMax = 9.5f;
+    private float yPos = 5.5f;
     //Enemy variables
     public int waveNumber = 1;
     public int enemyCount;
+    //powerup spawn locations
+    public GameObject[] spawns;
     // Start is called before the first frame update
     void Start()
     {
         SpawnEnemyWave(waveNumber);
+        //Spawns powerup at the start of the game
+        int index = UnityEngine.Random.Range(0, spawns.Length);
+        Instantiate(powerupPrefab, spawns[index].transform.position, powerupPrefab.transform.rotation);
     }
 
     // Update is called once per frame
@@ -27,9 +35,12 @@ public class SpawnManager : MonoBehaviour
         {
             waveNumber++;
             SpawnEnemyWave(waveNumber);
+            //New powerup every wave
+            int index = UnityEngine.Random.Range(0, spawns.Length);
+            Instantiate(powerupPrefab, spawns[index].transform.position, powerupPrefab.transform.rotation);
         }
     }
-
+    //Enemy spawn methods
     void SpawnEnemyWave(int enemiesToSpawn)
     {
         for (int i = 0; i < enemiesToSpawn; i++)
@@ -40,7 +51,7 @@ public class SpawnManager : MonoBehaviour
 
     public Vector2 SpawnPos()
     {
-         return new Vector2(Random.Range(xMin, xMax), yPos);
+         return new Vector2(UnityEngine.Random.Range(xMin, xMax), yPos);
     }
     
     public void SpawnEnemy()
